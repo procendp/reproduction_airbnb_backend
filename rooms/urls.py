85fd4a6dfc66767006fc2e7965c18a489d4c0f30
procendp <procendp@gmail.com>
2024-12-02 21:30:20 +0900

    made the url path for dividing

---
+++ rooms/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.see_all_rooms),       # "" : already rooms/
+    path("<int:room_pk>", views.see_one_room)
+    # path("<str:room_name>", views.blahblah---)
+
+]
