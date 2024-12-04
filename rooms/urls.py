b5c93dac7e4f7acf779523b0762bea1b72ff609c
procendp <procendp@gmail.com>
2024-12-04 23:52:54 +0900

    deleted HTML code, i will not make UI with django

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.see_all_rooms),       # "" : already rooms/
-    path("<int:room_pk>", views.see_one_room)
+    # path("", views.see_all_rooms),       # "" : already rooms/
+    # path("<int:room_pk>", views.see_one_room)
     # path("<str:room_name>", views.blahblah---)
 
 ]
