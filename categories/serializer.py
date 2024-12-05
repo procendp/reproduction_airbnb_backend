c4ac3d4e3cc5f5fa6807e908020d370b70ccded1
procendp <procendp@gmail.com>
2024-12-05 17:34:51 +0900

    CRUD with REST API serializer

--- categories/serializer.py
+++ categories/serializer.py
@@ -1,11 +1,23 @@
 from rest_framework import serializers
+from .models import Category
 
 # translate django to JSON for the browser
 
 class CategorySerializer(serializers.Serializer):
     
     # explaining something what Category have to serialzer
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
+    # from djnango DB model to JSON data
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, max_length=50)
+    kind = serializers.ChoiceField(choices=Category.CategoryKindChoices.choices)
     created_at = serializers.DateTimeField(read_only=True)      # read_only=True : serializer에게 user가 error 없이 보낼 수 있게 해줌.. is_valid에서 안 걸림
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):   
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+
+        return instance
--- categories/views.py
+++ categories/views.py
@@ -1,14 +1,13 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
-# from .serializers import CategorySerializer       # 왜 이거지?
 from .serializer import CategorySerializer
 
 ### ------ before use django REST framework ------ 
 # from django.http import JsonResponse
 # from django.core import serializers
-
-# Create your views here.
 
 @api_view(["GET", "POST"])
 def categories(request):
@@ -21,7 +20,8 @@ def categories(request):
         serializer = CategorySerializer(data=request.data)      # from user to serializer
         
         if serializer.is_valid:
-            return Response({'created': True})
+            new_category = serializer.save()
+            return Response(CategorySerializer(new_category).data)
         else:
             return Response(serializer.errors)
     
@@ -29,8 +29,23 @@ def categories(request):
     # all_categories = Category.objects.all()     # queryset
     # return JsonResponse({'ok': True, 'categories': serializers.serialize("json", all_categories)})      # queryset을 Json으로 serialize
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)       # from django to Json
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+    
+    if request.method == "GET":
+        serializer = CategorySerializer(category)       # from django to Json
+        return Response(serializer.data)                # 해당 데이터 응답으로 보내줌
+    elif request.method == "PUT":
+        serializer = CategorySerializer(category, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.error)
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status= HTTP_204_NO_CONTENT)
