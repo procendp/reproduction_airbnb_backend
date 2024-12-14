bb4bf30180154959f1b2eb3b7a5979b9c347de48
procendp <procendp@gmail.com>
2024-12-14 21:08:34 +0900

    changed to upload method using URL

--- medias/models.py
+++ medias/models.py
@@ -4,7 +4,8 @@ from common.models import CommonModel
 # Create your models here.
 class Photo(CommonModel):
 
-    file = models.ImageField()
+    file = models.URLField()        # 보안 위해 URLField로 대체
+    # file = models.ImageField()
     description = models.CharField(max_length=140)
     room = models.ForeignKey("rooms.Room", null=True, blank=True, on_delete=models.CASCADE, related_name="photos")
     experience = models.ForeignKey("experiences.Experience", null=True, blank=True, on_delete=models.CASCADE, related_name="photos")
@@ -14,7 +15,8 @@ class Photo(CommonModel):
 
 class Video(CommonModel):
 
-    file = models.FileField()
+    file = models.URLField()
+    # file = models.FileField()
     experience = models.OneToOneField("experiences.Experience", on_delete=models.CASCADE,related_name="videos" )
 
     def __str__(self):
