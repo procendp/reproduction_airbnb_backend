e811415ead04a446c1b9c65a4f0e44a46f96dd2d
procendp <procendp@gmail.com>
2024-12-15 23:08:10 +0900

    fixed the primary key

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -4,5 +4,5 @@ from .views import WishLists, WishlistDetail, WishlistToggle
 urlpatterns = [
     path("", WishLists.as_view()),
     path("<int:pk>", WishlistDetail.as_view()),
-    path("<int:pk>/rooms/<int:pk>", WishlistToggle.as_view()),
+    path("<int:pk>/rooms/<int:room_pk>", WishlistToggle.as_view()),
 ]
