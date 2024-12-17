11c9055d69b3f6c5d11821e86e46ae0418852bbe
procendp <procendp@gmail.com>
2024-12-17 23:44:12 +0900

    made user profile

--- users/models.py
+++ users/models.py
@@ -19,7 +19,7 @@ class User(AbstractUser):   # models.Model 상속 받지 않은 이유 : User를
     # first_name과 last_name을 쓰지 않을 계획임. -> django 내 소스를 수정하지말고, 여기서 오버라이딩 한번 해주면서 숨겨주자
     first_name = models.CharField(max_length=150, editable=False)
     last_name = models.CharField(max_length=150, editable=False)
-    avatar = models.ImageField(blank=True)        # poetry add Pillow 필요      # blank=True : 필수로 등록하지 않아도 되게끔
+    avatar = models.URLField(blank=True)        # poetry add Pillow 필요      # blank=True : 필수로 등록하지 않아도 되게끔
     name = models.CharField(max_length=150, default="")         # 기본 값 설정해줘야함, 기존에 등록돼있던 유저들에게도 컬럼을 부여하는데, 뭘 부여할지 django는 모르잖아
     is_host = models.BooleanField(default=False)     # null=True : T, F 가 아닌 Null로 채우기
     gender = models.CharField(max_length=10, choices=GenderChoices.choices)
---
+++ users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("me", views.Me.as_view()),
+]
