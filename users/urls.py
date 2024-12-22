3cbdafcd203e1999efbf8275d17262ee8da7f40b
procendp <procendp@gmail.com>
2024-12-23 03:02:47 +0900

    JWT encode

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
-    path("log-in", views.LogIn.as_view()),
+    path("log-in", views.LogIn.as_view()),                      # How to Log-in (1) : Cookie log-in
     path("log-out", views.LogOut.as_view()),
-    path("token-login", obtain_auth_token),     # 여기서 username과 password를 보내면 token 반환
+    path("token-login", obtain_auth_token),                     # How to Log-in (2) :  Auth Token log-in --> username과 password를 보내면 token 반환
+    path("jwt-login", views.JWTLogIn.as_view()),                #  How to Log-in (3) : JWT Log-in
     path("@<str:username>", views.PublicUser.as_view()),
 ]
