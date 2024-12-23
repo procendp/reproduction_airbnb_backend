f189b9bde98d056730c39d306c83c599979d990b
procendp <procendp@gmail.com>
2024-12-24 00:02:51 +0900

    how to read .env file with django-environ

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_##%rx@eq^9dfquk4cn)zb#_je=c-btf&^7&1%6czyt925an2c'
+# SECRET_KEY = 'django-insecure-_##%rx@eq^9dfquk4cn)zb#_je=c-btf&^7&1%6czyt925an2c'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
