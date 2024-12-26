73f1559a70689e69037a71c1483c67e7bb33a1fe
procendp <procendp@gmail.com>
2024-12-26 22:50:48 +0900

    set the simple test system and checked OK or FAILED

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+# just simple test
+class TestAmenities(APITestCase):
+    def test_two_plus_two(self):        # test_ 로 시작해야함 꼭
+        self.assertEqual(2 + 2, 4, "The math is wrong.")
