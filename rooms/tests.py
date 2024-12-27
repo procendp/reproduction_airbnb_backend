291a9991c91100bc69923bb147654a8ed9a6a8e6
procendp <procendp@gmail.com>
2024-12-28 00:31:50 +0900

    detail test about amenities

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,9 +1,7 @@
 from rest_framework.test import APITestCase
 from . import models
 
-# just simple test
 class TestAmenities(APITestCase):
-    # def test_two_plus_two(self):        # test_ 로 시작해야함 꼭
         
     NAME = "Amenity Test"
     DESC = "Amenity Description"
@@ -14,6 +12,7 @@ class TestAmenities(APITestCase):
             name=self.NAME,
             description=self.DESC,
         )
+
     def test_all_amenities(self):
         response = self.client.get(self.URL)
         data = response.json()
@@ -67,3 +66,111 @@ class TestAmenities(APITestCase):
         data = response.json()
         self.assertEqual(response.status_code, 400)
         self.assertIn("name", data)
+
+class TestAmenity(APITestCase):
+
+    NAME = "Test Amenity"
+    DESC = "Test Dsc"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+        response = self.client.get("/api/v1/rooms/amenities/1")
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+        )
+
+    def test_put_amenity(self):
+        updated_amenity_name = "Updated Amenity"
+        updated_amenity_description = "Updated Amenity desc"
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": updated_amenity_name,
+                "description": updated_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+        self.assertEqual(
+            data["name"],
+            updated_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            updated_amenity_description,
+        )
+
+        updated_amenity_name = "Updated Amenity 2"
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": updated_amenity_name,
+            },
+        )
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            updated_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            updated_amenity_description,
+        )
+
+        updated_amenity_description = "Updated Amenity desc 2"
+
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "description": updated_amenity_description,
+            },
+        )
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            updated_amenity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            updated_amenity_description,
+        )
+
+        invalid_name = "name" * 40
+        invalid_desc = "desc" * 40
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": invalid_name,
+                "description": invalid_desc,
+            },
+        )
+        self.assertEqual(
+            response.status_code,
+            400,
+        )
+
+    def test_delete_amenity(self):
+        response = self.client.delete("/api/v1/rooms/amenities/1")
+        self.assertEqual(response.status_code, 204)        
--- rooms/views.py
+++ rooms/views.py
@@ -49,7 +49,7 @@ class AmenityDetail(APIView):
             updated_amenity = serializer.save()
             return Response(AmenitySerializer(updated_amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
         
 
     def delete(self, request, pk):
