716c6963210e91d5fc86c1e75dfc1595fa78c2a0
procendp <procendp@gmail.com>
2024-12-28 00:43:57 +0900

    test about authentication

--- rooms/views.py
+++ rooms/views.py
@@ -91,7 +91,7 @@ class Rooms(APIView):
                     raise ParseError("Amenity not found")
 
             else:
-                return Response(serializer.errors)
+                return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     
 class RoomDetail(APIView):
 
